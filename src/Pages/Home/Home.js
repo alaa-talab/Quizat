6b7b45947fb7e3bdf2808f2efffd893298abc7cc
Alaa <alaatalab99@gmail.com>
2023-11-01 18:30:49 +0300

    commit 2

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,33 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import "./Home.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 
 const Home = () => {
+  const history = useHistory();
 
-    const history = useHistory();
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
 
-    const navigateToQuiz = () => {
-      history.push('/Body'); // Redirect to the '/Body' route (MainQuizPage)
-    };
-    const navigateToRusme = () => {
-      history.push('/Homepage'); // Redirect to the '/Body' route (MainQuizPage)
-    };
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+  });
 
+  const [signupData, setSignupData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const showLogin = () => {
+    setShowLoginModal(true);
+  };
+
+  const hideLogin = () => {
+    setShowLoginModal(false);
+  };
+
+  const showSignup = () => {
+    setShowSignupModal(true);
+  };
+
+  const hideSignup = () => {
+    setShowSignupModal(false);
+  };
+
+  const handleLogin = () => {
+    // Add your actual login/authentication logic here.
+    // For simplicity, we'll just display the entered data.
+    console.log('Logging in with data:', loginData);
+    hideLogin(); // Close the login modal
+  };
+
+  const handleSignup = () => {
+    // Add your actual signup/authentication logic here.
+    // For simplicity, we'll just display the entered data.
+    console.log('Signing up with data:', signupData);
+    hideSignup(); // Close the signup modal
+  };
+
+  const navigateToQuiz = () => {
+    history.push('/Body');
+  };
+
+  const navigateToRusme = () => {
+    history.push('/Homepage');
+  };
+  const navigateToHome = () => {
+    history.push('/');
+  };
   return (
     <div>
       {/* Content for the Home component */}
       {/* Navigation bar */}
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="#">
-            Your Website Name
-          </a>
+        <div
+  className="navbar-brand"
+  onClick={navigateToHome}
+  style={{ cursor: 'pointer' }}
+  role="link"
+>
+<img src="/Quizat-01 1.png" className="banner" alt="quiz app" />
+</div>
+          {/* Login Button */}
+          <button onClick={showLogin} className="btn btn-primary">
+            Login
+          </button>
+
+          {/* Signup Button */}
+          <button onClick={showSignup} className="btn btn-primary">
+            Sign Up
+          </button>
         </div>
       </nav>
 
       {/* Hero section */}
       <div className="jumbotron text-center">
-        <h1>Welcome to Your Website</h1>
+        <h1>Welcome to Quizat</h1>
         <p>Your one-stop destination for quizzes and resume building</p>
       </div>
 
@@ -41,8 +105,8 @@ const Home = () => {
                 Test your knowledge with a wide range of quizzes on various topics. Have fun while learning!
               </p>
               <button onClick={navigateToQuiz} className="btn btn-primary">
-          Take a Quiz
-        </button>
+                Take a Quiz
+              </button>
             </div>
           </div>
           <div className="col-md-6">
@@ -52,8 +116,8 @@ const Home = () => {
                 Create a professional resume that stands out. Land your dream job with our easy-to-use resume builder.
               </p>
               <button onClick={navigateToRusme} className="btn btn-primary">
-              Build Resume
-        </button>
+                Build Resume
+              </button>
             </div>
           </div>
         </div>
@@ -61,8 +125,101 @@ const Home = () => {
 
       {/* Footer */}
       <footer className="bg-dark text-light text-center py-3">
-        &copy; 2023 Your Website Name
+        &copy; 2023 Quizat Team
       </footer>
+
+      {/* Login Modal */}
+      <div className={`modal ${showLoginModal ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: showLoginModal ? 'block' : 'none' }}>
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Login</h5>
+              <button type="button" className="close" onClick={hideLogin} aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Username"
+                  value={loginData.username}
+                  onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  value={loginData.password}
+                  onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+                />
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" onClick={hideLogin}>
+                Close
+              </button>
+              <button type="button" className="btn btn-primary" onClick={handleLogin}>
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+   {/* Signup Modal */}
+<div className={`modal ${showSignupModal ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: showSignupModal ? 'block' : 'none' }}>
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title">Sign Up</h5>
+        <button type="button" className="close" onClick={hideSignup} aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            value={signupData.username}
+            onChange={(e) => setSignupData({ ...signupData, username: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"  
+            className="form-control"
+            placeholder="Email"
+            value={signupData.email}
+            onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            value={signupData.password}
+            onChange={(e) => setSignupData({ ...signupData, password: e.target.value })}
+          />
+        </div>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" onClick={hideSignup}>
+          Close
+        </button>
+        <button type="button" className="btn btn-primary" onClick={handleSignup}>
+          Sign Up
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
